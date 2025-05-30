25d7cda5aecc923795906ea763838b4a05b30530
abiraj-01 <yoghanraj1002@gmail.com>
2025-05-30 09:26:15 +0530

    v1.0: Initial version

---
+++ Student Grade Tracker/Student Grade Tracker/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Student_Grade_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstGrades = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStudent = new System.Windows.Forms.TextBox();
+            this.btnAddGrade = new System.Windows.Forms.Button();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstGrades
+            // 
+            this.lstGrades.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstGrades.FormattingEnabled = true;
+            this.lstGrades.ItemHeight = 25;
+            this.lstGrades.Location = new System.Drawing.Point(377, 20);
+            this.lstGrades.Name = "lstGrades";
+            this.lstGrades.Size = new System.Drawing.Size(208, 129);
+            this.lstGrades.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Grade";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Student Name";
+            // 
+            // txtStudent
+            // 
+            this.txtStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudent.Location = new System.Drawing.Point(221, 24);
+            this.txtStudent.Name = "txtStudent";
+            this.txtStudent.Size = new System.Drawing.Size(150, 30);
+            this.txtStudent.TabIndex = 8;
+            // 
+            // btnAddGrade
+            // 
+            this.btnAddGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddGrade.Location = new System.Drawing.Point(169, 168);
+            this.btnAddGrade.Name = "btnAddGrade";
+            this.btnAddGrade.Size = new System.Drawing.Size(100, 72);
+            this.btnAddGrade.TabIndex = 7;
+            this.btnAddGrade.Text = "Add Record";
+            this.btnAddGrade.UseVisualStyleBackColor = true;
+            this.btnAddGrade.Click += new System.EventHandler(this.btnAddGrade_Click);
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrade.Location = new System.Drawing.Point(221, 71);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(150, 30);
+            this.txtGrade.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 261);
+            this.Controls.Add(this.lstGrades);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.txtStudent);
+            this.Controls.Add(this.btnAddGrade);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstGrades;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStudent;
+        private System.Windows.Forms.Button btnAddGrade;
+        private System.Windows.Forms.TextBox txtGrade;
+
+    }
+}
+
---
+++ Student Grade Tracker/Student Grade Tracker/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Grade_Tracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddGrade_Click(object sender, EventArgs e)
+        {
+            string student = txtStudent.Text;
+            string grade = txtGrade.Text;
+            lstGrades.Items.Add(student + " - " + grade);
+            txtStudent.Clear();
+            txtGrade.Clear();
+
+        }
+
+       
+    }
+}
