dd66e369f7531a07d68a3cb9166f9e5805ceaa7a
abiraj-01 <yoghanraj1002@gmail.com>
2025-05-30 09:53:58 +0530

    v1.1: Bug fixes validation

--- Student Grade Tracker/Student Grade Tracker/Form1.Designer.cs
+++ Student Grade Tracker/Student Grade Tracker/Form1.Designer.cs
@@ -34,25 +34,30 @@
             this.txtStudent = new System.Windows.Forms.TextBox();
             this.btnAddGrade = new System.Windows.Forms.Button();
             this.txtGrade = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // lstGrades
             // 
             this.lstGrades.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstGrades.FormattingEnabled = true;
-            this.lstGrades.ItemHeight = 25;
-            this.lstGrades.Location = new System.Drawing.Point(377, 20);
+            this.lstGrades.ItemHeight = 29;
+            this.lstGrades.Location = new System.Drawing.Point(503, 25);
+            this.lstGrades.Margin = new System.Windows.Forms.Padding(4);
             this.lstGrades.Name = "lstGrades";
-            this.lstGrades.Size = new System.Drawing.Size(208, 129);
+            this.lstGrades.Size = new System.Drawing.Size(276, 149);
             this.lstGrades.TabIndex = 12;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(40, 74);
+            this.label2.Location = new System.Drawing.Point(53, 91);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 25);
+            this.label2.Size = new System.Drawing.Size(82, 29);
             this.label2.TabIndex = 9;
             this.label2.Text = "Grade";
             // 
@@ -60,26 +65,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(40, 27);
+            this.label1.Location = new System.Drawing.Point(53, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 25);
+            this.label1.Size = new System.Drawing.Size(175, 29);
             this.label1.TabIndex = 10;
             this.label1.Text = "Student Name";
             // 
             // txtStudent
             // 
             this.txtStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtStudent.Location = new System.Drawing.Point(221, 24);
+            this.txtStudent.Location = new System.Drawing.Point(295, 30);
+            this.txtStudent.Margin = new System.Windows.Forms.Padding(4);
             this.txtStudent.Name = "txtStudent";
-            this.txtStudent.Size = new System.Drawing.Size(150, 30);
+            this.txtStudent.Size = new System.Drawing.Size(199, 36);
             this.txtStudent.TabIndex = 8;
             // 
             // btnAddGrade
             // 
             this.btnAddGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddGrade.Location = new System.Drawing.Point(169, 168);
+            this.btnAddGrade.Location = new System.Drawing.Point(225, 207);
+            this.btnAddGrade.Margin = new System.Windows.Forms.Padding(4);
             this.btnAddGrade.Name = "btnAddGrade";
-            this.btnAddGrade.Size = new System.Drawing.Size(100, 72);
+            this.btnAddGrade.Size = new System.Drawing.Size(133, 89);
             this.btnAddGrade.TabIndex = 7;
             this.btnAddGrade.Text = "Add Record";
             this.btnAddGrade.UseVisualStyleBackColor = true;
@@ -88,22 +96,50 @@
             // txtGrade
             // 
             this.txtGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtGrade.Location = new System.Drawing.Point(221, 71);
+            this.txtGrade.Location = new System.Drawing.Point(295, 87);
+            this.txtGrade.Margin = new System.Windows.Forms.Padding(4);
             this.txtGrade.Name = "txtGrade";
-            this.txtGrade.Size = new System.Drawing.Size(150, 30);
+            this.txtGrade.Size = new System.Drawing.Size(199, 36);
             this.txtGrade.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(408, 207);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 89);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(589, 207);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 89);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 261);
+            this.ClientSize = new System.Drawing.Size(997, 500);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lstGrades);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtGrade);
             this.Controls.Add(this.txtStudent);
             this.Controls.Add(this.btnAddGrade);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -119,7 +155,9 @@
         private System.Windows.Forms.TextBox txtStudent;
         private System.Windows.Forms.Button btnAddGrade;
         private System.Windows.Forms.TextBox txtGrade;
-
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- Student Grade Tracker/Student Grade Tracker/Form1.cs
+++ Student Grade Tracker/Student Grade Tracker/Form1.cs
@@ -27,6 +27,46 @@ namespace Student_Grade_Tracker
 
         }
 
-       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string student = txtStudent.Text.Trim();
+            string gradeText = txtGrade.Text.Trim();
+
+            if (string.IsNullOrEmpty(student) || string.IsNullOrEmpty(gradeText))
+            {
+                MessageBox.Show("Please enter both student name and grade.");
+                return;
+            }
+            int grade;
+            if (!int.TryParse(gradeText, out grade) || grade < 0 || grade > 100)
+            {
+                MessageBox.Show("Grade must be a number between 0 and 100.");
+                return;
+            }
+
+            lstGrades.Items.Add(student + " - " + grade);
+            txtStudent.Clear();
+            txtGrade.Clear();
+
+
+        }
+
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            if (lstGrades.SelectedIndex != -1)
+            {
+                lstGrades.Items.RemoveAt(lstGrades.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Please select an entry to delete.");
+            }
+        }
     }
 }
